909550674d7323a2a5f0e0900571f7b85ff2fc59
mike4747 <mike4747@oh.rr.com>
2020-12-12 20:16:51 -0500

    Removed the api functions from getInitialProps in _app.js.

--- pages/_app.js
+++ pages/_app.js
@@ -7,14 +7,10 @@ import Header from '../components/header/header';
 import Navbar from '../components/navbar/navbar';
 import Footer from '../components/footer';
 
-import { getSettings } from '../lib/api/settings';
-
 import '../styles/my_style.css';
 import '../styles/app_style.css';
 
-function MyApp({ Component, pageProps, settings }) {
-    console.log('settings:', settings);
-
+function MyApp({ Component, pageProps }) {
     const [loading, setLoading] = useState(false);
 
     Router.onRouteChangeStart = () => setLoading(true);
@@ -34,26 +30,12 @@ function MyApp({ Component, pageProps, settings }) {
 MyApp.propTypes = {
     Component: PropTypes.func,
     pageProps: PropTypes.any,
-    settings: PropTypes.object,
 };
 
 export default MyApp;
 
 export async function getInitialProps(context) {
     const baseApiUrl = 'http://localhost:3000';
-    // try {
-    //     const settingsResponse = await fetch(`${baseApiUrl}/api/settings`);
-    //     const settingsJson = await settingsResponse.json();
-    //     const settings = settingsJson[0] || {};
-
-    //     const settingsResponse = await getSettings();
-    //     console.log('settingsResponse:', settingsResponse);
-    //     if (settingsResponse && settingsResponse.length === 1) return { props: { settings: JSON.parse(JSON.stringify(settingsResponse[0])) } };
-    //     return { props: { settings: null } };
-    // } catch (error) {
-    //     console.error(error.message);
-    //     return { props: { settings: null } };
-    // }
 
     return {
         props: {},

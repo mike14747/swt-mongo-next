7bf7a2597fd7bffa1816c1df888dc33a77dd83ac
mike14747 <mike4747@oh.rr.com>
2021-08-12 23:37:34 -0400

    Tweaked some styling in HeaderNav.module.css.

--- components/NavDropdown.js
+++ components/NavDropdown.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-import styles from '../styles/navDropdown.module.css';
+import styles from '../styles/NavDropdown.module.css';
 
 export default function NavDropdown({ buttonText, listItems }) {
     return (

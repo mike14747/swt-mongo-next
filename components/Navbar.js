20516ef158b69559bc4e0b238b4a2ab9ddf7b434
mike14747 <mike4747@oh.rr.com>
2021-08-18 22:48:10 -0400

    Added functionality to the results dropdown in Navbar... though the destination pages aren't yet built.

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,17 +4,12 @@ import NavButton from './NavButton';
 import styles from '../styles/Navbar.module.css';
 
 const NavBar = ({ currentSeasonId, displaySchedule, storesInNavbar }) => {
-    console.log('displaySchedule in Navbar:', displaySchedule);
-    const tempContent = [
-        {
-            href: '/',
-            text: 'Brunswick (Monday)',
-        },
-        {
-            href: '/',
-            text: 'Mentor (Thursday)',
-        },
-    ];
+    const resultsStoresInNavbar = storesInNavbar?.map(store => {
+        return {
+            href: '/results/' + store.seasonId + '/store/' + store.storeId + '/division/' + store.divisionId,
+            text: store.storeCity + ' (' + store.divisionName + ')',
+        };
+    });
 
     const scheduleStoresInNavbar = storesInNavbar?.map(store => {
         return {
@@ -26,7 +21,7 @@ const NavBar = ({ currentSeasonId, displaySchedule, storesInNavbar }) => {
     return (
         <nav className={styles.nav + ' container'}>
             <NavButton buttonText="Standings" href={'/standings/' + currentSeasonId} />
-            <NavDropdown buttonText="Results" listItems={tempContent} />
+            <NavDropdown buttonText="Results" listItems={resultsStoresInNavbar} />
             {displaySchedule &&
                 <NavDropdown buttonText="Schedule" listItems={scheduleStoresInNavbar} />
             }

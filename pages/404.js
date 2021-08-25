610b29cc2b10c3d0cd25dd106c6b732ca262e848
mike14747 <mike4747@oh.rr.com>
2021-08-25 18:41:43 -0400

    Updated the 404 page.

--- pages/404.js
+++ pages/404.js
@@ -1,26 +1,37 @@
+import Head from 'next/head';
+
 const NoMatch = () => {
     return (
-        <section id="no-match">
-            <h2 className="error">
-                Error 404!
-            </h2>
-            <p>An error has occurred.</p>
-            <p>The page you are looking for does not exist!</p>
-            <style jsx>{`
-                #no-match {
-                    margin: 1.5rem;
-                }
+        <>
+            <Head>
+                <title>Page Not Found</title>
+            </Head>
 
-                p {
-                    font-size: 120%;
-                }
+            <section id="no-match">
+                <h2 className="error">
+                    Error 404!
+                </h2>
+                <p>An error has occurred.</p>
+                <p>The page you are looking for does not exist!</p>
+                <style jsx>{`
+                    #no-match {
+                        max-width: 80ch;
+                        margin: 0 auto;
+                    }
 
-                .error {
-                    color: #dc3545;
-                    margin-bottom: 1rem;
-                }
-            `}</style>
-        </section>
+                    p {
+                        font-size: clamp(1rem, 0.55vw + 0.55rem, 1.2rem);
+                    }
+
+                    .error {
+                        font-size: clamp(1.4rem, 1vw + 0.9rem, 2.1rem);
+                        color: #dc3545;
+                        margin-bottom: 1rem;
+                    }
+                `}</style>
+            </section>
+        </>
+
     );
 };
 

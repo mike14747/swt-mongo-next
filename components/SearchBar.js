30a74e35965b9d68e61b5145f0e403a0f4bb5d2f
mike14747 <mike4747@oh.rr.com>
2021-09-09 12:41:04 -0400

    Added aria-hidded true to homepage news item images. Commented out the search text label in the SearchBar.

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -26,7 +26,7 @@ const SearchBar = () => {
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="search" className={styles.searchLabel}>
-                <span className={styles.searchText}>Search:</span>
+                {/* <span className={styles.searchText}>Search:</span> */}
                 <input
                     id="search"
                     type="text"
